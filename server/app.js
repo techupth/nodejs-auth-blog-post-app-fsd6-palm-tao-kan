e5e8e5e921d88e0f453fd1accdd2edbe34a4f5a9
Krittayapornn <krittayaporn.ww@gmail.com>
2024-07-01 15:31:17 +0700

    create API register / login (back-end)

--- server/app.js
+++ server/app.js
@@ -3,16 +3,19 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import postRouter from "./apps/posts.js";
 import { client } from "./utils/db.js";
+import authRouter from "./apps/auth.js";
+import dotenv from "dotenv";
 
 async function init() {
   const app = express();
   const port = 4000;
-
+  dotenv.config();
   await client.connect();
 
   app.use(cors());
   app.use(bodyParser.json());
   app.use("/posts", postRouter);
+  app.use(authRouter);
 
   app.get("/", (req, res) => {
     res.send("Hello World!");
